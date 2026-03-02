58340b1444993b30aa9f3ca0caf3958d9d369837
salim <aaqibalam290@gmail.com>
2026-03-02 17:17:19 +0530

    learning react STATES

---
+++ react-state-class/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Counter from './Counter';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>
+      states in react using vite
+    </h1>
+
+      <Counter />
+      </>
+  )
+}
+
+export default App
